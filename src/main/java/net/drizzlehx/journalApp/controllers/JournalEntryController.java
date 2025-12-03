85a07317bd8c43e53118e9440015287c7cf02f36
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-12-03 18:47:11 +0530

    getters and setters

--- src/main/java/net/drizzlehx/journalApp/controllers/JournalEntryController.java
+++ src/main/java/net/drizzlehx/journalApp/controllers/JournalEntryController.java
@@ -1,9 +1,29 @@
 package net.drizzlehx.journalApp.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController
 {
+    private Map<Long,JournalEntry> journalEntries = new HashMap<>();
+    @GetMapping("/abc")
+    public List<JournalEntry> getall()
+    {
+        return new ArrayList<>(journalEntries.values());
+    }
+    @GetMapping("/def")
+    public List<JournalEntry> delall()
+    {
+        return new ArrayList<>(journalEntries.values());
+    }
+
 
 }
