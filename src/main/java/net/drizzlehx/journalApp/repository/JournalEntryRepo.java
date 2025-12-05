fa4a311851b981d68b69011bc673528246f7d7bf
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-12-05 14:00:54 +0530

    v2 - saved entry in db

--- src/main/java/net/drizzlehx/journalApp/repository/JournalEntryRepo.java
+++ src/main/java/net/drizzlehx/journalApp/repository/JournalEntryRepo.java
@@ -1,12 +1,13 @@
 package net.drizzlehx.journalApp.repository;
 
-import net.drizzlehx.journalApp.entity.journalEntry;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
+
+import net.drizzlehx.journalApp.entity.journalEntry;
 
 @Component
 public interface JournalEntryRepo extends MongoRepository<journalEntry,String>
 {
 
 }
+        
