7a44968843f9c9aabeef37be9be04a2c0c28cd62
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-12-05 14:14:32 +0530

    v2 - updated global error handling enpoint

--- src/main/java/net/drizzlehx/journalApp/controllers/JournalEntryControllerv2.java
+++ src/main/java/net/drizzlehx/journalApp/controllers/JournalEntryControllerv2.java
@@ -1,14 +1,19 @@
 package net.drizzlehx.journalApp.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import net.drizzlehx.journalApp.entity.journalEntry;
 import net.drizzlehx.journalApp.service.JournalEntryService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/journal")
@@ -29,6 +34,12 @@ public class JournalEntryControllerv2
         journalEntryService.save(myEntry);
         return null;
     }
+    // @GetMapping("/error")
+    // @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    // public String errorExample()
+    // {
+    //     return "Sample error endpoint";
+    // }
     @GetMapping("/id/{myId}")
     public journalEntry getJournalEntry(@PathVariable long myId)
 
@@ -46,5 +57,5 @@ public class JournalEntryControllerv2
     {
         return null;
     }
-
 }
+
