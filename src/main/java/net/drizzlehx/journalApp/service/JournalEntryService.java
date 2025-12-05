718aaa86f5022aade1cb15fb69003d30e2d54cfd
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-12-05 14:34:46 +0530

    v2 - minor fixes

--- src/main/java/net/drizzlehx/journalApp/service/JournalEntryService.java
+++ src/main/java/net/drizzlehx/journalApp/service/JournalEntryService.java
@@ -2,9 +2,13 @@ package net.drizzlehx.journalApp.service;
 
 import net.drizzlehx.journalApp.entity.journalEntry;
 import net.drizzlehx.journalApp.repository.JournalEntryRepo;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.List;
+import java.util.Optional;
 
 
 @Component
@@ -20,5 +24,10 @@ public class JournalEntryService
     public List<journalEntry> getAll()
     {
         journalEntryRepo.findAll();
+    }
+
+    public Optional<journalEntry> getbyId(@PathVariable ObjectId myId)
+    {
+        return Optional.of(journalEntryRepo.findById(myId).get());
     }
 }
