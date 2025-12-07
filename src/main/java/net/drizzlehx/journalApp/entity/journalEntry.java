e9d6d775204aaa9b376651fcabcc851c8099ddc8
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-12-07 16:13:22 +0530

    v2 - installed dependencies

--- src/main/java/net/drizzlehx/journalApp/entity/journalEntry.java
+++ src/main/java/net/drizzlehx/journalApp/entity/journalEntry.java
@@ -1,5 +1,7 @@
 package net.drizzlehx.journalApp.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,36 +12,12 @@ import java.util.Date;
 @Document(collection = "journal_entries")
 public class journalEntry
 {
+    @Getter
+    @Setter
     @Id
     private ObjectId id;
     private String title;
     private Date date;
     private String content;
-
-    public Date getDate() {
-        return date;
-    }
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-    public String getContent() {
-        return content;
-    }
-    public void setContent(String content) {
-        this.content = content;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public ObjectId getId() {
-        return id;
-    }
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
 
 }
